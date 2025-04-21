c35e7f34c3e27afe22b6b3f1811a6800a7dc6979
Alexandra Butler <butler0alex@gmail.com>
2025-04-21 16:59:27 -0400

    Adding button to navigate to original screen

--- src/components/ViewListComponent.jsx
+++ src/components/ViewListComponent.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
 import './../style.css';
 
 const ViewListComponent = () => {
+    const navigate = useNavigate();
     const [listItems, setListItems] = useState([]);
 
     const addListsToPage = async () => {
@@ -13,14 +15,20 @@ const ViewListComponent = () => {
         lists.data.map((item) => {console.log(item);});
     };
 
+    const handleViewItem = () => {
+        navigate('/exercise-app/');
+    };
+
     // Loads lists from database to page when page loads
     useEffect(() => { addListsToPage(); }, []);
 
     return (
-        <div className="main_container" style={{margin:0, padding:0}}>
-            <section className="workout-list" 
-                style={{overflowX:'hidden'}}>
+        <div className="main_container" style={{margin:0, padding:0, overflowX:'hidden'}}>
+            <section className="workout-list" style={{overflowX:'hidden'}}>
             <h1>Workout Lists</h1>
+            <button onClick={handleViewItem} className="btn-view" style={{marginBottom: '1rem', color: '#ff7043'}}>
+                Search for Workouts
+            </button>
             {listItems.length > 0 && (
                 <ul className="search-dropdown" style={{position: 'relative', margin:'2rem', width:'90%'}}>
                 {listItems.map((item) => (
