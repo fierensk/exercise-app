e80b131117df3c8daffc00995df82d6d553288d7
Kate <katefierens@gmail.com>
2025-04-24 17:17:33 -0400

    update test with new routing to account for new list functionality

--- src/components/ExerciseComponent.test.jsx
+++ src/components/ExerciseComponent.test.jsx
@@ -1,10 +1,15 @@
-import { render, screen } from '@testing-library/react'
-import { describe, it, expect} from 'vitest'
-import ExerciseComponent from './ExerciseComponent'
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import ExerciseComponent from './ExerciseComponent';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('ExerciseComponent', () => {
   it('should render the header', () => {
-    render(<ExerciseComponent />);
+    render(
+      <MemoryRouter>
+        <ExerciseComponent />
+      </MemoryRouter>
+    );
     const heading = screen.getByText('Workout List');
     expect(heading).toBeInTheDocument();
   });
